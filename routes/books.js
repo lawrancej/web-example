75190cda277055aabea9a64fcd34be2cc4b34c1f
Joey Lawrance <lawrancej@wit.edu>
2016-03-01 11:44:27 -0500

    Fixed local testing.

--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
+var pg = require('pg').native;
 
 /* GET home page. */
 router.get('/', function(req, response, next) {
+  console.log(process.env.DATABASE_URL);
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM book', function(err, result){
+    client.query('SELECT * FROM book', function(err, result) {
       done();
       if (err) {
-        response.json(process.env);
+        response.json(process.env.DATABASE_URL);
       } else {
         response.json(result.rows);
       }
