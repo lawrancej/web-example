2f6935395730f72ecae69496fbd079aafbe143b8
Joey Lawrance <lawrancej@wit.edu>
2016-03-02 01:29:48 -0500

    Authentication example.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -10,6 +9,57 @@ var users = require('./routes/users');
 var books = require('./routes/books');
 
 var app = express();
+
+var session = require('express-session');
+
+var bcrypt = require('bcryptjs');
+var pg = require('pg').native;
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+  usernameField: 'username',
+  passwordField: 'password'
+},
+function(username, password, done) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, next) {
+    console.log("Connected to database");
+    client.query('SELECT * FROM users WHERE username = $1', [username], function(err, result) {
+      next();
+      if (err) {
+        console.log("Database error");
+        return done(err);
+      }
+      if (result.rows) {
+        var matched = bcrypt.compareSync(password, result.rows[0].password);
+        if (matched) {
+          console.log("Successful login");
+          return done(null, result.rows[0]);
+        }
+      }
+      console.log("Bad username or password");
+      return done(null, false);
+    });
+  });
+}));
+
+// Store user information into session
+passport.serializeUser(function(user, done) {
+  return done(null, user.id);
+});
+
+// Get user information out of session
+passport.deserializeUser(function(id, done) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, next) {
+    client.query('SELECT id, username FROM users WHERE id = $1', [id], function(err, result) {
+      next();
+      if (result) {
+        return done(null, result.rows[0]);
+      }
+      return done(null, false);
+    });
+  });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +70,16 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'web example',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: app.get('env') === 'production' }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/books', books);
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,42 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('user');
 });
+
+router.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+router.post('/login',
+  passport.authenticate('local', { failureRedirect: 'login' }),
+  function(req, res,next) {
+    // res.json(req.user);
+    res.redirect('profile');
+  });
+
+router.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+function loggedIn(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('login');
+  }
+}
+
+router.get('/profile',
+  loggedIn,
+  function(req, res){
+      res.render('profile', { user: req.user });
+  });
 
 module.exports = router;
