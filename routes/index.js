78f2f19d560ac2a0c86fe76d03b6f6c1ac8be02c
Joey Lawrance <lawrancej@wit.edu>
2016-03-21 10:18:35 -0400

    Session example.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/session', function(req, res) {
+  var sess = req.session;
+  if (sess.views) {
+    sess.views++;
+  } else {
+    sess.views = 1;
+  }
+  res.render('index', { title: 'You visited this ' + sess.views + ' times'});
+});
+
 module.exports = router;
